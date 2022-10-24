ca51c225103da7502dc922d8c3d1934a01a2dee3
Alen Boric <boricalen@live.com>
2022-10-25 01:04:45 +0200

    EmployeeApplication

--- src/main/java/com/employee/EmployeeApplication/controller/EmployeeController.java
+++ src/main/java/com/employee/EmployeeApplication/controller/EmployeeController.java
@@ -5,9 +5,7 @@ import com.employee.EmployeeApplication.entity.Employee;
 import com.employee.EmployeeApplication.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,6 +29,22 @@ public class EmployeeController {
 
 
     }
+    @RequestMapping("/employees/{id}")
+    public Employee findAnEmployee(@PathVariable int id){
+      return  employeeService.getAnEmployee(id);
+
+    }
+    @RequestMapping(value = "/employees",method = RequestMethod.POST)
+    public void createEmployee(@RequestBody Employee employee){
+        employeeService.createEmployee(employee);
+    }
+    @RequestMapping(value = "/employees/{id}",method = RequestMethod.PUT)
+    public void updateEmployee(@PathVariable int id,@RequestBody Employee employee){
+         employeeService.updateEmployee(employee);
+
+    }
+
+
 
 
 }
--- src/main/java/com/employee/EmployeeApplication/service/EmployeeService.java
+++ src/main/java/com/employee/EmployeeApplication/service/EmployeeService.java
@@ -4,15 +4,16 @@ package com.employee.EmployeeApplication.service;
 import com.employee.EmployeeApplication.entity.Employee;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    List<Employee> employeeList = Arrays.asList(
+    List<Employee> employeeList = new ArrayList<>(Arrays.asList(
             new Employee(1, "First Employee", "Zagreb"),
             new Employee(2, "Second Employee", "Osijek")
-    );
+    ));
 
 
 
@@ -22,5 +23,25 @@ public class EmployeeService {
     }
 
 
+    public Employee getAnEmployee (int id){
+        return employeeList.stream().filter(e ->(e.getEmployeeId() == id)).findFirst().get();
+    }
+
+   public void createEmployee (Employee employee){
+        employeeList.add(employee);
+   }
+   public void updateEmployee(Employee employee){
+        List <Employee> tempEmployee= new ArrayList<>();
+        for(Employee emp : employeeList ){
+            if(emp.getEmployeeId() == employee.getEmployeeId()){
+                emp.setEmployeeName(employee.getEmployeeName());
+                emp.setEmployeeCity(employee.getEmployeeCity());
+            }
+              tempEmployee.add(emp);
+        }
+
+         this.employeeList = tempEmployee;
+
+    }
 
 }
