85faad7729c015bf178e6ca421f168699122844a
Alen Boric <boricalen@live.com>
2022-10-28 01:39:33 +0200

    EmployeeApplication

--- src/main/java/com/employee/EmployeeApplication/service/EmployeeService.java
+++ src/main/java/com/employee/EmployeeApplication/service/EmployeeService.java
@@ -35,7 +35,7 @@ public class EmployeeService {
        employeeRepository.save(employee);
    }
    public void updateEmployee(Employee employee){
-        List <Employee> tempEmployee= new ArrayList<>();
+      /*  List <Employee> tempEmployee= new ArrayList<>();
         for(Employee emp : employeeList ){
             if(emp.getEmployeeId() == employee.getEmployeeId()){
                 emp.setEmployeeName(employee.getEmployeeName());
@@ -45,18 +45,24 @@ public class EmployeeService {
         }
 
          this.employeeList = tempEmployee;
+     */
+       employeeRepository.save(employee);
 
     }
 
     public void deleteEmployee(int id){
-        List <Employee> tempEmployee= new ArrayList<>();
+       /* List <Employee> tempEmployee= new ArrayList<>();
          for(Employee emp : employeeList){
             if(emp.getEmployeeId() == id)
                 continue;
             tempEmployee.add(emp);
             }
 
-            this.employeeList = tempEmployee;
+            this.employeeList = tempEmployee;*/
+
+        employeeRepository.delete(employeeRepository.getById(id));
+
+
          }
 
 
