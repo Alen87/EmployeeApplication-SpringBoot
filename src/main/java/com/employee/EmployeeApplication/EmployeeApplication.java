f83f29e8eb32b5d7e47b05dd284a6e426fd51e7c
Alen Boric <boricalen@live.com>
2022-10-31 15:50:38 +0100

    EmployeeApplication

--- src/main/java/com/employee/EmployeeApplication/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication/EmployeeApplication.java
@@ -1,7 +1,12 @@
 package com.employee.EmployeeApplication;
 
+import com.employee.EmployeeApplication.entity.Address;
+import com.employee.EmployeeApplication.entity.Project;
+import com.employee.EmployeeApplication.service.EmployeeService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeApplication {
@@ -10,5 +15,17 @@ public class EmployeeApplication {
 
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
+
+
+
+	@Bean
+	public CommandLineRunner initialCreate(EmployeeService employeeService){
+	                   return(args) ->{
+						   Address address1 = new Address("Line 1","Line 2","ZipCode 1","City1","State1","Country1);
+						   Project project1 = new Project("Name 1","Client")
+		};
+	}
+
+
 
 }
